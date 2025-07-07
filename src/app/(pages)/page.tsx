c37ac4d9008d6f66f265cba2e0528599ebb9d3a3
yongbin kim <kyb@agencygo.net>
2025-07-07 09:56:28 +0900

    🔧 chore: 색상 변수 정의(primary, secondary, destructive, muted)

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -10,6 +10,18 @@ export default function Home() {
       <h1 className="text-4xl font-bold">hello world</h1>
       <h1 className="text-4xl font-extrabold">hello world</h1>
       <h1 className="text-4xl font-black">hello world</h1>
+      <button className="bg-primary hover:bg-primary-hover active:bg-primary-active text-primary-foreground px-4 py-2">
+        Primary 버튼
+      </button>
+      <button className="bg-secondary hover:bg-secondary-hover active:bg-secondary-active text-secondary-foreground px-4 py-2">
+        Secondary 버튼
+      </button>
+      <button className="bg-destructive hover:bg-destructive-hover active:bg-destructive-active text-destructive-foreground px-4 py-2">
+        Destructive 버튼
+      </button>
+      <button className="bg-muted hover:bg-muted-hover active:bg-muted-active text-muted-foreground px-4 py-2">
+        Muted 버튼
+      </button>
     </div>
   );
 }
