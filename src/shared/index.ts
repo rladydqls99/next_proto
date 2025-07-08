b33afc11a6fa96bafefe4365da829b93721b23f3
yongbin kim <kyb@agencygo.net>
2025-07-08 13:57:26 +0900

    ♻️ refactor: index.ts 파일에서 내보내는 항목 정리 및 타입 내보내기 추가

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,13 +1,23 @@
+// api -------------------------------------------------------------
 export { apiClient } from "./api/http-client";
 
-export { cn } from "./lib/cn";
-export { getNavItemByUrl, type NavItem, type NavItemInfo } from "./lib/navigation";
-
-export { Button, PrimaryButton, SecondaryButton, DestructiveButton } from "./ui/button";
-export { Input } from "./ui/input";
-export { Table } from "./ui/table";
-
-export { ROUTE_PATH, navItems } from "./config/navigation-config";
+// config -------------------------------------------------------------
+export { ROUTE_PATH } from "./config/navigation-config";
+export { navItems } from "./config/navigation-config";
 export { ENDPOINT } from "./config/endpoint-config";
 
-export { type YesOrNo, type ResponseType } from "./model/common-type";
+// lib -------------------------------------------------------------
+export { cn } from "./lib/cn";
+export { getNavItemByUrl } from "./lib/navigation";
+export type { NavItem, NavItemInfo } from "./lib/navigation";
+
+// model -------------------------------------------------------------
+export type { YesOrNo, ResponseType } from "./model/common-type";
+
+// ui -------------------------------------------------------------
+export { Button } from "./ui/button";
+export { PrimaryButton } from "./ui/button";
+export { SecondaryButton } from "./ui/button";
+export { DestructiveButton } from "./ui/button";
+export { Input } from "./ui/input";
+export { Table } from "./ui/table";
